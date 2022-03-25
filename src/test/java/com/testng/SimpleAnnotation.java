431398d6c8fc5507ba1f4bd234e087adc6fa248b
Niranjanv0208-hub <vniranjan0208@gmail.com>
2022-03-25 09:57:32 +0530

    automation

---
+++ src/test/java/com/testng/SimpleAnnotation.java
@@ -0,0 +1,20 @@
+package com.testng;
+
+import org.testng.annotations.Test;
+
+public class SimpleAnnotation {
+
+	@Test
+	private void userName() {
+		// TODO Auto-generated method stub
+       System.out.println("username");
+	}
+	
+	@Test
+	private void password() {
+		// TODO Auto-generated method stub
+
+		System.out.println("password");
+	}
+	
+}
